32e3db0e0b75888465adb1d7766736040bc213d2
semihycl_1 <190541019@firat.edu.tr>
2022-10-01 14:35:44 +0300

    panel edited

--- database/migrations/2022_10_01_073125_create_launchpad_table.php
+++ database/migrations/2022_10_01_073125_create_launchpad_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('launchpad', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('location');
+            $table->string('name')->nullable();
+            $table->string('location')->nullable();
             $table->timestamps();
         });
     }
